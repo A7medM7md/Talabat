5cc58e784fb65c5bdcbe68cc608e949944a49bf4
A7medM7md <ahmedelgebaly2511@gmail.com>
2026-07-22 13:52:36 +0300

    feat: add AllowAll CORS policy and update middleware usage

--- Talabat.APIS/Program.cs
+++ Talabat.APIS/Program.cs
@@ -1,110 +1,123 @@
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Talabat.APIS.Errors;
 using Talabat.APIS.Extension_Methods;
 using Talabat.APIS.Middlewares;
-using Talabat.APIS.Profiles;
 using Talabat.Core.Models;
-using Talabat.Core.Repositories;
-using Talabat.Repository;
 using Talabat.Repository.Data;
 using Talabat.Repository.Identity;
 
 namespace Talabat.APIS
 {
-	public class Program
-	{
-		public static async Task Main(string[] args)
-		{
-			#region Create Host
-			var builder = WebApplication.CreateBuilder(args);
-			#endregion
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            #region Create Host
+            var builder = WebApplication.CreateBuilder(args);
+            #endregion
 
-			#region DI Container
-			// Add services to the container.
+            #region DI Container
+            // Add services to the container.
 
-			builder.Services.AddControllers();
-			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen();
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
-			#region Extension Methods
-			// Configure Entity Framework Core
-			builder.Services.AddConnectionStrings(builder.Configuration);
+            #region Extension Methods
+            // Configure Entity Framework Core
+            builder.Services.AddConnectionStrings(builder.Configuration);
 
-			// Configure Redis
-			builder.Services.AddRedisConnectionString(builder.Configuration);
+            // Configure Redis
+            builder.Services.AddRedisConnectionString(builder.Configuration);
 
-			// Configure the Application Services
-			builder.Services.AddApplicationServices(builder.Configuration);
+            // Configure the Application Services
+            builder.Services.AddApplicationServices(builder.Configuration);
 
-			// Configure Identity Services
-			builder.Services.AddIdentityServices(builder.Configuration);
-			#endregion
+            // Configure Identity Services
+            builder.Services.AddIdentityServices(builder.Configuration);
+            #endregion
 
-			#endregion
+            #region CORS
 
-			#region Build Project
-			var app = builder.Build();
-			#endregion
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder
+                        .AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
 
-			#region Update Database [Apply Migrations]
-			using var scope = app.Services.CreateScope();
-			var services = scope.ServiceProvider;
-			var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+            #endregion
 
-			try
-			{
-				// Update Main Database
-				var dbContext = services.GetRequiredService<AppDbContext>();
-				await dbContext.Database.MigrateAsync();
+            #endregion
 
-				// Update Identity Database
-				var IdentityDbContext = services.GetRequiredService<ApplicationIdentityDbContext>();
-				await IdentityDbContext.Database.MigrateAsync();
+            #region Build Project
+            var app = builder.Build();
+            #endregion
 
-				// Seeding Data (Main Database)
-				await DataSeedInitializer.SeedDataAsync(dbContext);
 
-				// Seeding Data (Identity)
-				var userManger = services.GetRequiredService<UserManager<AppUser>>();
-				await IdentityDataSeedInitializer.SeedUserAsync(userManger);
-			}
-			catch (Exception ex)
-			{
-				var logger = loggerFactory.CreateLogger<Program>();
-				logger.LogError(ex, "An Error Occurred During Appling The Migration");
-			}
-			#endregion
+            #region Update Database [Apply Migrations]
+            using var scope = app.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
 
-			#region Middlewares
-			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
-			{
-				// Exception handling middleware to determine if the request will passed to the next middleware
-				app.UseMiddleware<ExceptionMiddleware>();
+            try
+            {
+                // Update Main Database
+                var dbContext = services.GetRequiredService<AppDbContext>();
+                await dbContext.Database.MigrateAsync();
 
-				// Extension Method
-				app.UseSwaggerMiddlewares();
-			}
+                // Update Identity Database
+                var IdentityDbContext = services.GetRequiredService<ApplicationIdentityDbContext>();
+                await IdentityDbContext.Database.MigrateAsync();
 
-			app.UseStatusCodePagesWithRedirects("/errors/{0}");
+                // Seeding Data (Main Database)
+                await DataSeedInitializer.SeedDataAsync(dbContext);
 
-			app.UseHttpsRedirection();
+                // Seeding Data (Identity)
+                var userManger = services.GetRequiredService<UserManager<AppUser>>();
+                await IdentityDataSeedInitializer.SeedUserAsync(userManger);
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(ex, "An Error Occurred During Appling The Migration");
+            }
+            #endregion
 
-			app.UseStaticFiles();
+            #region Middlewares
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                // Exception handling middleware to determine if the request will passed to the next middleware
+                app.UseMiddleware<ExceptionMiddleware>();
 
-			app.UseCors("MyPolicy");
+                // Extension Method
+                app.UseSwaggerMiddlewares();
+            }
 
-			app.UseAuthentication();
+            app.UseCors("AllowAll");
 
-			app.UseAuthorization();
+            app.UseStatusCodePagesWithRedirects("/errors/{0}");
 
-			app.MapControllers();
-			#endregion
+            app.UseHttpsRedirection();
 
-			app.Run();
-		}
-	}
+            app.UseStaticFiles();
+
+            app.UseCors("MyPolicy");
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+            #endregion
+
+            app.Run();
+        }
+    }
 }
